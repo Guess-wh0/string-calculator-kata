5dad0bbe3745cdd68706b43c64ca8333e746cfba
Guess-wh0 <abhisheksinghrawat.9@gmail.com>
2024-05-03 12:34:34 +0530

    added logic for sum of numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,6 @@
 class StringCalculator
+  def add(expression='')
+    return 0 if expression.empty?
+    expression.split(',').map(&:to_i).sum
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper'
 
 # checks for StringCalculator class
 describe StringCalculator do
-  it 'can create new object' do
-    calc = StringCalculator.new
+  calc = StringCalculator.new
+
+  context '#add' do
+    it "should return 0 on empty string" do
+      expect(calc.add("")).to eq(0)
+    end
+
+    it "should return 6 on '1,2,3'" do
+      expect(calc.add("1,2,3")).to eq(6)
+    end
+
   end
 end

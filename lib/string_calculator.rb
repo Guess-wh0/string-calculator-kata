ff693fa9663bd39c97fb05462e5ba6f10b4191d0
Guess-wh0 <abhisheksinghrawat.9@gmail.com>
2024-05-03 15:07:20 +0530

    added format validation and expression cleaning

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,18 @@
 class StringCalculator
+  INVALID_FORMATS = [/,[\n]*\z/, /,\n*,/, /[^0-9\n,]/].freeze
+
+  private def valid?(expression= '')
+    INVALID_FORMATS.none? { |exp| exp.match?(expression) }
+  end
+
   def add(expression='')
+    # clean expression from empty spaces
+    expression.gsub!(' ', '')
     return 0 if expression.empty?
+
+    unless valid?(expression)
+      raise ArgumentError, 'Invalid format'
+    end
     expression.split(',').map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,13 +5,44 @@ describe StringCalculator do
   calc = StringCalculator.new
 
   context '#add' do
-    it "should return 0 on empty string" do
-      expect(calc.add("")).to eq(0)
+    it 'should return 0 on empty string' do
+      expect(calc.add('')).to eq(0)
     end
 
-    it "should return 6 on '1,2,3'" do
-      expect(calc.add("1,2,3")).to eq(6)
+    it 'should return 4 on "4"' do
+      expect(calc.add('4')).to eq(4)
     end
 
+    it 'should return 6 on "1,2,3"' do
+      expect(calc.add('1,2,3')).to eq(6)
+    end
+
+    it 'should return correct sum for string with \n b/w numbers "1,2,\n3,4"' do
+      expect(calc.add("1,2,\n3,4"))
+    end
+
+    it 'raises FormatError for "1,2,4,"' do
+      expect { calc.add("1,2,4,") }.to raise_error(ArgumentError, 'Invalid format')
+    end
+
+    it 'raises FormatError for "1,2,4,\n\n\n"' do
+      expect { calc.add("1,2,4,\n\n\n") }.to raise_error(ArgumentError, 'Invalid format')
+    end
+
+    it 'raises FormatError for "1,2,4, \n\n\n  "' do
+      expect { calc.add("1,2,4, \n\n\n  ") }.to raise_error(ArgumentError, 'Invalid format')
+    end
+
+    it 'raises FormatError for "1,2,,4"' do
+      expect { calc.add("1,2,,4") }.to raise_error(ArgumentError, 'Invalid format')
+    end
+
+    it 'raises FormatError for "1,2,4,a"' do
+      expect { calc.add("1,2,4,a") }.to raise_error(ArgumentError, 'Invalid format')
+    end
+
+    it 'should return 7 for "1,2,4 \n\n\n  "' do
+      expect(calc.add("1,2,4 \n\n\n  ")).to eq(7)
+    end
   end
 end
